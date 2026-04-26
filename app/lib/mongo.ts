78d539856a0c896e532f651ff3ab22dd2eb58d4c
kettie <gjiniketrina@gmail.com>
2026-04-26 14:30:44 +0200

    Pipeline fix

--- app/lib/mongo.ts
+++ app/lib/mongo.ts
@@ -6,14 +6,18 @@ let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
+export default function getClientPromise() {
+  if (!uri) {
+    throw new Error("MONGODB_URI is missing");
+  }
+
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+
+  return global._mongoClientPromise;
 }
-
-clientPromise = global._mongoClientPromise;
-
-export default clientPromise;
